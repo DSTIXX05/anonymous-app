c28ddd9aaf88d68c259afb7f3003653ca293f2cd
Dstixx05 <delightsomeasolo@gmail.com>
2025-04-30 06:56:48 +0100

    port number updated to 3001

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ app.use(express.json());
 
 app.use('/me', userRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
 });
